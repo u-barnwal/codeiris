f3d1b965778ec189b842e624bc8039ff4270cca0
michaelspeed <michael.the.don@gmail.com>
2021-03-25 20:54:53 +0530

    add Dockerfile

--- pages/views/home.tsx
+++ pages/views/home.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { NextPageContext } from 'next';
 
-function Home() {
-  return <div>Hello World!</div>;
+function Home({ data }) {
+  return <div>Hello ____ {data.data}!</div>;
+}
+
+export async function getServerSideProps(ctx: NextPageContext) {
+  return {
+    props: {
+      data: ctx.query,
+    },
+  };
 }
 
 export default Home;
