6c6991a76f900b757d058599e469bb683bc78adb
michaelspeed <michael.the.don@gmail.com>
2021-03-26 22:09:01 +0530

    add graphql module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigService } from '@nestjs/config';
+import { GraphQLConfig } from '../config/config.interface';
+import { DateScalar } from '../common/scalar/date.scalar';
 
-@Module({})
+@Module({
+  imports: [
+    GraphQLModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => {
+        const gqlConfig = configService.get<GraphQLConfig>('graphql');
+        return {
+          autoSchemaFile: gqlConfig.schema,
+          debug: gqlConfig.debug,
+          playground: gqlConfig.playground,
+          buildSchemaOptions: {
+            numberScalarMode: 'integer',
+          },
+          context: ({ req }) => ({ req }),
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [DateScalar],
+})
 export class ApiModule {}
