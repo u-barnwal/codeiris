1615b79c8a13f20b578922e7d7d7552ed4f008e1
michaelspeed <michael.the.don@gmail.com>
2021-03-26 21:51:05 +0530

    add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import Next from 'next';
 import { RenderModule } from 'nest-next';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
   imports: [
     RenderModule.forRootAsync(
       Next({ dev: process.env.NODE_ENV !== 'production' }),
     ),
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
   ],
   controllers: [AppController],
   providers: [AppService],
