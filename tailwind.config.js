e77071980ee7b74e053ed1838e5c9ec7dbb34305
Utsav Barnwal <u.barnwal36@gmail.com>
2021-04-01 21:31:25 +0530

    add primary color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: {
+        light: '#7279da',
+        DEFAULT: '#3641CA',
+        dark: '#252c8d',
+      },
+      ...colors,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
