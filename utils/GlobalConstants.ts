d43982f601cf9198618d780db95e5b673282a51f
Risha Sringa Changmai <michael.the.don@gmail.com>
2021-04-18 00:27:33 +0530

    WIP build fix

--- utils/GlobalConstants.ts
+++ utils/GlobalConstants.ts
@@ -1,7 +1,7 @@
 export const refresh = 'refresh-token';
 console.log(process.env);
 export const host = process.env.NEXT_PUBLIC_SERVER_PATH;
-export const graphQLEndpoint = `${host}/graphql`;
+export const graphQLEndpoint = `${host ? host : 'https://dev.codeiris.dev'}/graphql`;
 export const refreshUrl = `${
   host ? host : 'https://dev.codeiris.dev'
 }/auth/refresh-token`;
