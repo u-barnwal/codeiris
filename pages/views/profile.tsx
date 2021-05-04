64ba049d8f363f9778a78c0c92728aae59c97e3c
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-05 02:49:36 +0530

    add animations on Profile

--- pages/views/profile.tsx
+++ pages/views/profile.tsx
@@ -18,6 +18,10 @@ import { SpinnerSize } from '../../lib/common/props/SpinnerProps';
 import Avatar from 'lib/components/atomic/avatar/Avatar';
 import Container from 'lib/components/atomic/containers/Container';
 import Tag from 'lib/components/Shared/Tag';
+import Animation from 'lib/components/Shared/Animation';
+
+import animationComment from './../static/animations/comment.json';
+import animationDocument from './../static/animations/document.json';
 
 function Profile() {
   const { data, loading } = useQuery<GetMeQuery, GetMeQueryVariables>(
@@ -69,14 +73,36 @@ function Profile() {
               <div className="flex-1 inline-block pl-5">
                 <div className="mb-3 text-xs text-right">STATS</div>
 
-                <div className="bg-gray-100 p-3 rounded-lg flex">
+                <div className="bg-gray-100 py-3 rounded-lg flex">
                   {[
-                    ['Posts', postCount.data.getMePosts.totalCount],
-                    ['Comments', commentCount.data.getMeComments.totalCount],
+                    [
+                      'Posts',
+                      postCount.data.getMePosts.totalCount,
+                      animationDocument,
+                    ],
+                    [
+                      'Comments',
+                      commentCount.data.getMeComments.totalCount,
+                      animationComment,
+                    ],
                   ].map((t, index) => (
-                    <div className="flex-1 text-right">
-                      <div className="text-2xl font-semibold">{t[1]}</div>
-                      <div className="text-sm">{t[0]}</div>
+                    <div
+                      className="flex-1 text-right flex items-center mx-5"
+                      key={index}
+                    >
+                      <Animation
+                        data={t[2]}
+                        height={48}
+                        width={48}
+                        loop={true}
+                      />
+
+                      <div className="flex-1"></div>
+
+                      <div>
+                        <div className="text-2xl font-semibold">{t[1]}</div>
+                        <div className="text-sm">{t[0]}</div>
+                      </div>
                     </div>
                   ))}
                 </div>
@@ -84,102 +110,6 @@ function Profile() {
             </div>
           </div>
         </Container>
-
-        // <main className="profile-page">
-        //   <section className="relative block" style={{ height: 500 }}>
-        //     <div
-        //       className="absolute top-0 w-full h-full bg-center bg-cover"
-        //       style={{
-        //         backgroundImage:
-        //           'url("https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80")',
-        //       }}
-        //     >
-        //       <span
-        //         id="blackOverlay"
-        //         className="w-full h-full absolute opacity-50 bg-black"
-        //       ></span>
-        //     </div>
-        //     <div
-        //       className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
-        //       style={{ height: 70 }}
-        //     >
-        //       <svg
-        //         className="absolute bottom-0 overflow-hidden"
-        //         xmlns="http://www.w3.org/2000/svg"
-        //         preserveAspectRatio="none"
-        //         version="1.1"
-        //         viewBox="0 0 2560 100"
-        //         x="0"
-        //         y="0"
-        //       >
-        //         <polygon
-        //           className="text-gray-300 fill-current"
-        //           points="2560 0 2560 100 0 100"
-        //         ></polygon>
-        //       </svg>
-        //     </div>
-        //   </section>
-        //   <section className="relative py-16 bg-gray-300">
-        //     <div className="container mx-auto px-4">
-        //       <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
-        //         <div className="px-6">
-        //           <div className="flex flex-wrap justify-center">
-        //             <div className="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-        //               <div className="relative">
-        //                 <Avatar size="large" label={data.me.firstName[0]} />
-        //               </div>
-        //             </div>
-        //           </div>
-        //           <div className="text-center mt-12">
-        //             <h3 className="text-4xl font-semibold leading-normal mb-2 text-gray-800">
-        //               {data.me.firstName} {data.me.middleName}{' '}
-        //               {data.me.lastName}
-        //             </h3>
-        //             <div className="w-full lg:w-4/12 px-4 lg:order-1">
-        //               <div className="flex justify-center py-4 lg:pt-4 pt-8">
-        //                 {postCount.data && (
-        //                   <div className="mr-4 p-3 text-center">
-        //                     <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-        //                       {postCount.data.getMePosts.totalCount}
-        //                     </span>
-        //                     <span className="text-sm text-gray-500">Post</span>
-        //                   </div>
-        //                 )}
-        //                 {commentCount.data && (
-        //                   <div className="lg:mr-4 p-3 text-center">
-        //                     <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-        //                       {commentCount.data.getMeComments.totalCount}
-        //                     </span>
-        //                     <span className="text-sm text-gray-500">
-        //                       Comments
-        //                     </span>
-        //                   </div>
-        //                 )}
-        //               </div>
-        //             </div>
-        //             <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
-        //               <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>
-        //               Placeholder Text
-        //             </div>
-        //             <div className="mb-2 text-gray-700 mt-10">
-        //               <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
-        //               Placeholder Text
-        //             </div>
-        //             <div className="mb-2 text-gray-700">
-        //               <i className="fas fa-university mr-2 text-lg text-gray-500"></i>
-        //               Placeholder Text
-        //             </div>
-        //           </div>
-        //           <div className="mt-10 py-10 border-t border-gray-300 text-center">
-        //             <div className="flex flex-wrap justify-center">
-        //               <div className="w-full lg:w-9/12 px-4"></div>
-        //             </div>
-        //           </div>
-        //         </div>
-        //       </div>
-        //     </div>
-        //   </section>
-        // </main>
       )}
     </div>
   );
