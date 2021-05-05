fd6ca353fc53c30b6f544aa474554ce0424ace29
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-05 19:10:40 +0530

    Restyle CommentListItem

--- lib/components/Comments/CommentListItem.tsx
+++ lib/components/Comments/CommentListItem.tsx
@@ -12,11 +12,13 @@ import Spinner from '../atomic/spinner';
 import { SpinnerSize } from '../../common/props/SpinnerProps';
 import clsx from 'clsx';
 import Button from '../atomic/button';
+import { MessageIcon } from '../Icons';
 
 interface CommentListItemProps {
   comment: any;
   child: boolean;
 }
+
 function CommentListItem({ comment, child }: CommentListItemProps) {
   const [add, setAdd] = useState(false);
   const [first, setFirst] = useState(1);
@@ -28,36 +30,24 @@ function CommentListItem({ comment, child }: CommentListItemProps) {
     skip: !child,
     pollInterval: 3000,
   });
+
   if (comment === undefined) {
     return <div />;
   }
+
   console.log(data);
+
   return (
     <div
-      className="w-full rounded rounded-lg overflow-hidden shadow-sm p-2 bg-white"
+      className="w-full rounded-lg overflow-hidden p-2 bg-white"
       key={comment.id}
     >
-      <div className="flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className={clsx({
-            'text-blueGray-500 mr-5': true,
-            'h-6 w-6': child,
-            'h-4 w-4': !child,
-          })}
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
-          />
-        </svg>
+      <div className="flex items-start">
+        <MessageIcon className="mr-4" color="#60606c" />
+
         <div>
           <div className="text-blueGray-700">{comment.body}</div>
+
           <div
             className="flex text-md text-blueGray-500"
             style={{ fontSize: 10 }}
@@ -79,27 +69,14 @@ function CommentListItem({ comment, child }: CommentListItemProps) {
                 className="flex items-center cursor-pointer"
                 onClick={() => setAdd(!add)}
               >
-                {' '}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4 mr-1"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                  />
-                </svg>{' '}
+                <MessageIcon className="mx-2" color="#60606c" size={4} />
                 Reply{' '}
               </div>
             )}
           </div>
         </div>
       </div>
+
       <Transition
         show={add}
         enter="transition-opacity duration-100"
@@ -109,15 +86,17 @@ function CommentListItem({ comment, child }: CommentListItemProps) {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div>
+        <div className="ml-10 mt-2 mb-5">
           <CommentsInput parentId={comment.id} />
         </div>
       </Transition>
+
       {loading && (
         <div className="flex justify-center items-center">
           <Spinner size={SpinnerSize.small} />
         </div>
       )}
+
       {data && data.getCommentsChildren.edges.length > 0 && (
         <div className="md:ml-10">
           {data.getCommentsChildren.edges.map((childcomments) => (
@@ -129,6 +108,7 @@ function CommentListItem({ comment, child }: CommentListItemProps) {
           ))}
         </div>
       )}
+
       {data && data.getCommentsChildren.totalCount > first && (
         <div className="flex justify-center items-center">
           <Button
@@ -140,11 +120,9 @@ function CommentListItem({ comment, child }: CommentListItemProps) {
               if (
                 data.getCommentsChildren.totalCount + 10 >
                 data.getCommentsChildren.totalCount
-              ) {
+              )
                 setFirst(first + 10);
-              } else {
-                setFirst(data.getCommentsChildren.totalCount);
-              }
+              else setFirst(data.getCommentsChildren.totalCount);
             }}
           >
             Load More
--- lib/components/Icons/index.tsx
+++ lib/components/Icons/index.tsx
@@ -161,4 +161,17 @@ export const CommentIcon: React.FC<IconProps> = (props) => {
   );
 };
 
+export const MessageIcon: React.FC<IconProps> = (props) => {
+  return (
+    <Icon {...props}>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+      />
+    </Icon>
+  );
+};
+
 export default Icon;
